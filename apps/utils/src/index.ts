69e09c786af111cae2addde330435f5ba57188c0
beambeambeam <supawitmarayat@gmail.com>
2025-01-20 19:39:54 +0700

    feat: watch openapi change

--- apps/utils/src/index.ts
+++ apps/utils/src/index.ts
@@ -1,18 +1,64 @@
+import { exec } from "child_process";
 import chokidar from "chokidar";
+import { promises as fs } from "fs";
 import * as path from "path";
+import ts from "typescript";
 
-const SERVER_PATH = "../../server/";
+const SERVER_PATH = "../server/openapi.json";
+const CLIENT_PATH = "./types.d.ts";
+
+const FILE = ts.factory.createTypeReferenceNode(
+  ts.factory.createIdentifier("File"),
+);
+const NULL = ts.factory.createLiteralTypeNode(ts.factory.createNull());
+
+function GetDate() {
+  const currentTime = new Date().toLocaleString();
+  return `[${currentTime}]`;
+}
 
 const fullPath = path.resolve(SERVER_PATH);
-console.log(`Full path to server directory: ${fullPath}`);
+console.log(`${GetDate()} Full path to server directory: ${fullPath}`);
 
-const watcher = chokidar.watch("../server/openapi.json");
+const watcher = chokidar.watch(SERVER_PATH);
 
 watcher.on("ready", () => {
-  console.log("Initial scan complete. Ready for changes");
+  console.log(`${GetDate()} Initial scan complete. Ready for changes`);
   console.log(watcher.getWatched());
 });
 
 watcher.on("change", async (path, stats) => {
-  console.log(`File ${path} has been changed`);
+  console.log(`${GetDate()} File ${path} has been changed`);
+  const fileContents = await fs.readFile(path, "utf-8");
+  console.log(`${GetDate()} File contents:\n${fileContents}`);
+  generateType();
 });
+
+async function generateType() {
+  exec(`prettier --write ${fullPath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing prettier: ${error}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Prettier stderr: ${stderr}`);
+      return;
+    }
+    console.log(`Prettier stdout: ${stdout}`);
+  });
+
+  exec(
+    `pnpm dlx openapi-typescript ${fullPath} -o ${CLIENT_PATH}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing openapi-typescript: ${error}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`openapi-typescript stderr: ${stderr}`);
+        return;
+      }
+      console.log(`openapi-typescript stdout: ${stdout}`);
+    },
+  );
+}
