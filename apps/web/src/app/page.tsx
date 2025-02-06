4ba0f2f91af7252e7425d76bacd70f1670cceb51
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2025-02-06 22:13:12 +0700

    Coming soon page (#52)
    Co-authored-by: yiiipunn <yiiipunn@gmail.com>
    Co-authored-by: SupeemAFK <Peemlomsak@gmai.com>

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,11 @@
-"use client";
+import ComingSoon from "@/components/ComingSoon";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  return <div className="font-prompt">Home</div>;
+  return (
+    <div className="font-prompt text-white w-screen h-screen xl:bg-[url(/bg-desktop.jpg)] bg-[url(/bg-mobile.jpg)] bg-cover bg-no-repeat bg-fixed bg-center">
+      <Navbar />
+      <ComingSoon />
+    </div>
+  );
 }
