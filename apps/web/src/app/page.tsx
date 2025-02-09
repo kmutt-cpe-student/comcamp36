ed058033212ab47c0077d4fb9c37644271930134
N0TAW00D <primsirikunawutnatthawat@gmail.com>
2025-02-09 22:34:05 +0700

    feat: redirect user using router.push
    Created #69

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,15 @@
-import ComingSoon from "@/components/ComingSoon";
-import Navbar from "@/components/Navbar";
-
 export default function Home() {
   return (
     <div className="font-prompt text-white w-screen h-screen xl:bg-[url(/bg-desktop.jpg)] bg-[url(/bg-mobile.jpg)] bg-cover bg-no-repeat bg-fixed bg-center">
-      <Navbar />
-      <ComingSoon />
+      <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-col items-center text-center">
+          <h1 className="text-3xl">You should not be here.</h1>
+          <br />
+          <a href="/comingsoon" className="underline text-[#F15A29] text-4xl">
+            Go Backkkkkkkk
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
