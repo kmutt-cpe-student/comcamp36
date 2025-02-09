5e594a639a0481dd2db79c238dd592d2a9036397
Natthawat Primsirikunawut <131734769+N0TAW00D@users.noreply.github.com>
2025-02-09 23:25:24 +0700

    Issue#69 comingsoon v1 (#80)

--- apps/web/src/app/comingsoon/page.tsx
+++ apps/web/src/app/comingsoon/page.tsx
@@ -1,7 +1,11 @@
+import ComingSoon from "@/components/ComingSoon";
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
     <div className="font-prompt text-white w-screen h-screen xl:bg-[url(/bg-desktop.jpg)] bg-[url(/bg-mobile.jpg)] bg-cover bg-no-repeat bg-fixed bg-center">
-      <div className="flex items-center justify-center h-screen"></div>
+      <Navbar />
+      <ComingSoon />
     </div>
   );
 }
---
+++ apps/web/src/libs/controlling.ts
@@ -0,0 +1,34 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+interface DateRedirectedProps {
+  deadline: string;
+}
+
+export default function RedirectHandler({ deadline }: DateRedirectedProps) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const allowedPath = "/comingsoon";
+
+  //   I can't found development status in .env. (NOT SURE ABOUT THIS.)
+  const isDevelopment = process.env.NODE_ENV === "development";
+
+  useEffect(() => {
+    const nowDate = new Date();
+    const endDate = new Date(deadline);
+
+    if (nowDate > endDate) {
+      return;
+    }
+
+    if (!isDevelopment) {
+      if (pathname !== allowedPath) {
+        router.push(allowedPath);
+      }
+    }
+  }, [pathname, router, deadline, isDevelopment]);
+
+  return null;
+}
