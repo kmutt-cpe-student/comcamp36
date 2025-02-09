e785df39c2f2aab6e1e9c6e9482d473abc548327
N0TAW00D <primsirikunawutnatthawat@gmail.com>
2025-02-09 23:21:50 +0700

    fix: delete-home-glitch

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,7 @@
 export default function Home() {
   return (
     <div className="font-prompt text-white w-screen h-screen xl:bg-[url(/bg-desktop.jpg)] bg-[url(/bg-mobile.jpg)] bg-cover bg-no-repeat bg-fixed bg-center">
-      <div className="flex items-center justify-center h-screen">
-        <div className="flex flex-col items-center text-center">
-          <h1 className="text-3xl">You should not be here.</h1>
-          <br />
-          <a href="/comingsoon" className="underline text-[#F15A29] text-4xl">
-            Go Backkkkkkkk
-          </a>
-        </div>
-      </div>
+      <div className="flex items-center justify-center h-screen"></div>
     </div>
   );
 }
