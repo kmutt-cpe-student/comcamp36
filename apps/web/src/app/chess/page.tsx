9ddc505e389f79b9c1ce4b55d1f801dc3f5ded6e
3raphat <96657413+3raphat@users.noreply.github.com>
2025-02-10 23:05:35 +0700

    test: chess demo page

---
+++ apps/web/src/app/chess/page.tsx
@@ -0,0 +1,13 @@
+import ChessGame from "@/components/chess/ChessGame";
+
+const ChessDemoPage = () => {
+  return (
+    <main className="container mx-auto py-8 px-4">
+      <div className="flex justify-center">
+        <ChessGame />
+      </div>
+    </main>
+  );
+};
+
+export default ChessDemoPage;
