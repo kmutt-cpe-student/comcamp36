536d7e380ed0d450202f5ef68939426ad1772cad
beambeambeam <supawitmarayat@gmail.com>
2025-02-13 14:46:45 +0700

    feat: extra footer

--- apps/web/src/components/navigate/footer.tsx
+++ apps/web/src/components/navigate/footer.tsx
@@ -1,9 +1,26 @@
+import Image from "next/image";
+
 function Footer() {
   return (
-    <div className="flex w-full items-center justify-center border-t border-t-white/10 py-2">
-      <p className="text-[1rem] text-white/50">
-        ©ComCamp36 All right reserved.
-      </p>
+    <div className="bg-charcoal-1 font-noto-sans-thai-looped h-fit w-full border-t border-t-white/10 bg-fixed bg-no-repeat p-10">
+      <div className="flex h-fit w-full flex-col gap-4">
+        <div className="flex w-[10rem] justify-center">
+          <Image
+            style={{ width: "100%", height: "auto" }}
+            width={550}
+            height={288}
+            src="/static/image/logo.svg"
+            alt="ComCamp36Logo"
+            priority
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="text-[1rem]">ComCamp36 All right reserved.</p>
+          <p className="text-[1rem]">
+            ©2025 Made with Love and Tear, CPE38 ❤️
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
