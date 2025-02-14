af20babb8734a6bd335fc616deb91445e763ab8f
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-02-14 11:51:57 +0700

    feat: seo (#96)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,8 +8,21 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Comcamp36",
-  description: "Camp by Computer Engineering Department, KMUTT",
+  title: "ComCamp 36",
+  description: `ค่าย ComCamp36 จากวิศวะคอมฯ บางมด ขอเชิญน้อง ๆ ม.ปลาย มาร่วมกัน "ติดปีก" ปลดล็อกสกิลในค่ายสุดเจ๋งที่มาพร้อมกับความรู้และความสนุก สมัครฟรี กางปีกให้พร้อมแล้วมาร่วมผจญภัยไปด้วยกัน!`,
+  openGraph: {
+    title: "ComCamp 36",
+    url: "https://comcamp.io",
+    images: `${process.env.CF_PAGES_BRANCH === "production" ? "https://comcamp.io" : `${process.env.CF_PAGES_URL}`}/og-image.webp`,
+    type: "website",
+    description: `ค่าย ComCamp36 จากวิศวะคอมฯ บางมด ขอเชิญน้อง ๆ ม.ปลาย มาร่วมกัน "ติดปีก" ปลดล็อกสกิลในค่ายสุดเจ๋งที่มาพร้อมกับความรู้และความสนุก สมัครฟรี กางปีกให้พร้อมแล้วมาร่วมผจญภัยไปด้วยกัน!`,
+  },
+  twitter: {
+    title: "ComCamp 36",
+    images: `${process.env.CF_PAGES_BRANCH === "production" ? "https://comcamp.io" : `${process.env.CF_PAGES_URL}`}/og-image.webp`,
+    description: `ค่าย ComCamp36 จากวิศวะคอมฯ บางมด ขอเชิญน้อง ๆ ม.ปลาย มาร่วมกัน "ติดปีก" ปลดล็อกสกิลในค่ายสุดเจ๋งที่มาพร้อมกับความรู้และความสนุก สมัครฟรี กางปีกให้พร้อมแล้วมาร่วมผจญภัยไปด้วยกัน!`,
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
