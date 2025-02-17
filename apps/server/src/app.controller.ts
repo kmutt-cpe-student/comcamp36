2699d2808c9e856326f2bbfb189fad4f228c29a4
beambeambeam <supawitmarayat@gmail.com>
2025-02-17 17:34:16 +0700

    chore: clean main

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,27 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiResponse } from '@nestjs/swagger';
-import {
-  CheckTelPayloadDto,
-  CheckTelResponseDto,
-  GetHelloResponseDto,
-} from './dto/app.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  @ApiResponse({ status: 200, type: GetHelloResponseDto })
-  getHello(): GetHelloResponseDto {
-    return {
-      id: 'test',
-    };
-  }
-
-  @Post()
-  @ApiResponse({ status: 200, type: CheckTelResponseDto, isArray: true })
-  checkTel(@Body() body: CheckTelPayloadDto) {
-    return this.appService.checkTel(body.tel);
-  }
 }
