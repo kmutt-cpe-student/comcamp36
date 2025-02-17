d33bbb31d4e541b855e115fca7a97eefe388e278
beambeambeam <supawitmarayat@gmail.com>
2025-02-17 18:32:35 +0700

    chore: clean more

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -1,18 +1,3 @@
 import { Injectable } from '@nestjs/common';
-import { CheckTelResponseDto } from './dto/app.dto';
-
 @Injectable()
-export class AppService {
-  getHello() {
-    return {
-      id: 'hellow',
-    };
-  }
-
-  checkTel(tel: string): CheckTelResponseDto {
-    return {
-      tel: tel,
-      check: true,
-    };
-  }
-}
+export class AppService {}
