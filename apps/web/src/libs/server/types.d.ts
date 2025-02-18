3e18a5d7f0754fc4db923d0f010138d6d39f8446
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2025-02-18 22:41:53 +0700

    feat: create users resource, fix schema and add class validator

--- apps/web/src/libs/server/types.d.ts
+++ apps/web/src/libs/server/types.d.ts
@@ -1,7 +1,91 @@
-export type paths = Record<string, never>;
+export interface paths {
+  "/": {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    get: operations["AppController_getHello"];
+    put?: never;
+    post?: never;
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
+  "/users": {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    get: operations["UsersController_findAll"];
+    put?: never;
+    post: operations["UsersController_create"];
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
+  "/users/{id}": {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    get: operations["UsersController_findOne"];
+    put?: never;
+    post?: never;
+    delete: operations["UsersController_remove"];
+    options?: never;
+    head?: never;
+    patch: operations["UsersController_update"];
+    trace?: never;
+  };
+}
 export type webhooks = Record<string, never>;
 export interface components {
-  schemas: never;
+  schemas: {
+    CreateUserDto: {
+      google_id: string;
+      email: string;
+    };
+    UpdateUserDto: {
+      fullname: string;
+      age: number;
+      /** Format: date-time */
+      birth: Date;
+      gender: string;
+      religion: string;
+      blood_group: string;
+      graduation: string;
+      school: string;
+      course: string;
+      telephone: string;
+      medical_coverage: string;
+      chronic_diseas: string;
+      self_medicine: string;
+      drug_allergic: string;
+      food_allergic: string;
+      perfer_food: string;
+      address: string;
+      home_phone_tel: string;
+      comcamp_attendance: boolean;
+      size: string;
+      everyday_attendence: boolean;
+      has_laptop: boolean;
+      travel: string;
+      parent_fullname: string;
+      parent_relation: string;
+      parent_phone: string;
+      has_submit_answer: boolean;
+    };
+  };
   responses: never;
   parameters: never;
   requestBodies: never;
@@ -9,4 +93,121 @@ export interface components {
   pathItems: never;
 }
 export type $defs = Record<string, never>;
-export type operations = Record<string, never>;
+export interface operations {
+  AppController_getHello: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+    };
+  };
+  UsersController_findAll: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+    };
+  };
+  UsersController_create: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody: {
+      content: {
+        "application/json": components["schemas"]["CreateUserDto"];
+      };
+    };
+    responses: {
+      201: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+    };
+  };
+  UsersController_findOne: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path: {
+        id: string;
+      };
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+    };
+  };
+  UsersController_remove: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path: {
+        id: string;
+      };
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+    };
+  };
+  UsersController_update: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path: {
+        id: string;
+      };
+      cookie?: never;
+    };
+    requestBody: {
+      content: {
+        "application/json": components["schemas"]["UpdateUserDto"];
+      };
+    };
+    responses: {
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+    };
+  };
+}
