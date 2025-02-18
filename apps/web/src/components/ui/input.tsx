f64b3b58008a613e6987a39a750d9920c0ea3344
beambeambeam <supawitmarayat@gmail.com>
2025-02-18 15:38:57 +0700

    style: input

--- apps/web/src/components/ui/input.tsx
+++ apps/web/src/components/ui/input.tsx
@@ -5,7 +5,15 @@ import { cn } from "@/libs/utils";
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
     return (
-      <input type={type} className={cn("", className)} ref={ref} {...props} />
+      <input
+        type={type}
+        className={cn(
+          "flex h-9 w-full rounded-md border border-white/20 bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:hidden placeholder:text-white/40 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white/20 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
     );
   },
 );
