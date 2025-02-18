3f77bb7200ff3c84312195d2a87855b8c55f8c67
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2025-02-18 22:41:18 +0700

    feat: create users resource, fix schema and add class validator

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from 'nestjs-pino';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { LoggerModule } from 'nestjs-pino';
         },
       },
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
