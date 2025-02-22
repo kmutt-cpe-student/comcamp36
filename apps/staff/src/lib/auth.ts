d02581405e15654989cb32bb7fa6550721f6ebad
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-02-23 05:43:25 +0700

    ci: pls work

--- apps/staff/src/lib/auth.ts
+++ apps/staff/src/lib/auth.ts
@@ -3,11 +3,16 @@ import Database from "better-sqlite3";
 import { admin, username } from "better-auth/plugins"
 import { nextCookies } from "better-auth/next-js";
 
-const isCI = (process.env.CI === "true")
-const dbPath = isCI ? ":memory:" : (process.env.NODE_ENV === "production" ? "./sqlite/sqlite.db" : "./staff.db")
+const dbPath = () => {
+    if (process.env.CI === "true") return ":memory:";
+    
+    return process.env.NODE_ENV === "production" 
+      ? "./app/sqlite/sqlite.db" 
+      : "./staff.db";
+  }
 
 export const auth = betterAuth({
-    database: new Database(dbPath),
+    database: new Database(dbPath()),
     emailAndPassword: {
         enabled: true,
     },
