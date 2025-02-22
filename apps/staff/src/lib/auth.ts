98a3b487688da1d31b33c9bb0e2d3e26adfaebf9
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-02-23 04:26:00 +0700

    ci: use in memory db for github actions?

--- apps/staff/src/lib/auth.ts
+++ apps/staff/src/lib/auth.ts
@@ -3,7 +3,8 @@ import Database from "better-sqlite3";
 import { admin, username } from "better-auth/plugins"
 import { nextCookies } from "better-auth/next-js";
 
-const dbPath = (process.env.NODE_ENV === "production") ? "./sqlite/sqlite.db" : "../../staff.db"
+const isCI = (process.env.CI === "true")
+const dbPath = isCI ? ":memory:" : (process.env.NODE_ENV === "production" ? "./sqlite/sqlite.db" : "../../staff.db")
  
 export const auth = betterAuth({
     database: new Database(dbPath),
