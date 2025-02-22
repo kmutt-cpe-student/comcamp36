b2cb02acf499be481808cf94bdeda67df2abc6fc
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-02-22 19:10:46 +0700

    feat: sponsor grid (#118)

---
+++ apps/web/src/components/landing/components/sponsor-logo-renderer.tsx
@@ -0,0 +1,30 @@
+import { MagicCard } from "@/components/card/magic-card";
+import { Sponsor, SponsorTiers } from "../sponsor";
+
+interface SponsorLogoRendererProps {
+  data: Sponsor;
+}
+
+const SponsorLogoRenderer = ({ data: s }: SponsorLogoRendererProps) => {
+  let h = 60;
+
+  if (s.tier === SponsorTiers.diamond) {
+    h = 160;
+  } else if (s.tier === SponsorTiers.platinum) {
+    h = 100;
+  }
+
+  return (
+    <MagicCard className="flex w-full justify-center border-white/20 px-6 py-2">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        style={{ width: "auto", height: `${h}px` }}
+        src={s.image_path}
+        alt={`${s.name} logo`}
+        loading="lazy"
+      />
+    </MagicCard>
+  );
+};
+
+export default SponsorLogoRenderer;
---
+++ apps/web/src/components/landing/components/sponsor-subgrid.tsx
@@ -0,0 +1,27 @@
+import { Sponsor, SponsorTiers } from "../sponsor";
+import SponsorLogoRenderer from "./sponsor-logo-renderer";
+
+interface SponsorSubgridProps {
+  data: Sponsor[];
+  tier: SponsorTiers;
+}
+
+export default function SponsorSubgrid({ data, tier }: SponsorSubgridProps) {
+  const amt = data.filter((sp) => sp.tier === tier).length;
+  const gridClass =
+    amt === 1
+      ? "md:grid-cols-1"
+      : amt === 2
+        ? "md:grid-cols-2"
+        : "md:grid-cols-3";
+
+  return (
+    <div className={`grid grid-cols-1 ${gridClass} gap-x-4 gap-y-4`}>
+      {data
+        .filter((sp) => sp.tier === tier)
+        .map((sp) => (
+          <SponsorLogoRenderer data={sp} key={sp.name} />
+        ))}
+    </div>
+  );
+}
