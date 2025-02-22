3ce2d577a5ae766d0026b58fc489a8dd98ed1175
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-02-22 22:16:25 +0700

    fix: datepicker color (pt.2)

--- apps/web/src/components/ui/calendar.tsx
+++ apps/web/src/components/ui/calendar.tsx
@@ -23,27 +23,26 @@ function Calendar({
     nav: "absolute top-0 flex w-full justify-between z-10",
     button_previous: cn(
       buttonVariants({ variant: "ghost" }),
-      "size-9 p-0 text-zinc-400/80 dark:hover:text-zinc-50",
+      "size-9 p-0 text-zinc-400/80 hover:text-zinc-50",
     ),
     button_next: cn(
       buttonVariants({ variant: "ghost" }),
       "size-9 p-0 text-zinc-400/80 hover:text-zinc-50",
     ),
     weekday: "size-9 p-0 text-xs font-medium text-white",
-    day_button:
-      "relative flex size-9 items-center justify-center whitespace-nowrap " +
-      "rounded-md p-0 text-zinc-50 group-[[data-selected]:not(.range-middle)]:[transition-property:color,background-color,border-radius,box-shadow] " +
-      "group-[[data-selected]:not(.range-middle)]:duration-150 group-data-disabled:pointer-events-none focus-visible:z-10 hover:not-in-data-selected:bg-zinc-800 " +
-      "group-data-selected:bg-zinc-50 hover:not-in-data-selected:text-zinc-50 group-data-selected:text-zinc-900 group-data-disabled:text-zinc-50/30 group-data-disabled:line-through " +
-      "group-data-outside:text-zinc-50/30 group-data-selected:group-data-outside:text-zinc-900 outline-none focus-visible:ring-zinc-300/50 focus-visible:ring-[3px] " +
-      "group-[.range-start:not(.range-end)]:rounded-e-none group-[.range-end:not(.range-start)]:rounded-s-none group-[.range-middle]:rounded-none " +
-      "group-[.range-middle]:group-data-selected:bg-zinc-800 group-[.range-middle]:group-data-selected:text-zinc-50",
+    day_button: `relative flex size-9 items-center justify-center whitespace-nowrap 
+      rounded-md p-0 text-zinc-50 group-[[data-selected]:not(.range-middle)]:[transition-property:color,background-color,border-radius,box-shadow] 
+      group-[[data-selected]:not(.range-middle)]:duration-150 group-data-disabled:pointer-events-none focus-visible:z-10 hover:not-in-data-selected:bg-zinc-800 
+      group-data-selected:bg-zinc-50 hover:not-in-data-selected:text-zinc-50 group-data-selected:text-zinc-900 group-data-disabled:text-zinc-50/30 group-data-disabled:line-through 
+      group-data-outside:text-zinc-50/30 group-data-selected:group-data-outside:text-zinc-900 outline-none focus-visible:ring-zinc-300/50 focus-visible:ring-[3px] 
+      group-[.range-start:not(.range-end)]:rounded-e-none group-[.range-end:not(.range-start)]:rounded-s-none group-[.range-middle]:rounded-none 
+      group-[.range-middle]:group-data-selected:bg-zinc-800 group-[.range-middle]:group-data-selected:text-zinc-50`,
     day: "group size-9 px-0 py-px text-sm",
     range_start: "range-start",
     range_end: "range-end",
     range_middle: "range-middle",
     today:
-      "*:after:pointer-events-none *:after:absolute *:after:bottom-1 *:after:start-1/2 *:after:z-10 *:after:size-[3px] *:after:-translate-x-1/2 *:after:rounded-full *:after:bg-zinc-900 [&[data-selected]:not(.range-middle)>*]:after:bg-white [&[data-disabled]>*]:after:bg-zinc-950/30 *:after:transition-colors dark:*:after:bg-zinc-50 dark:[&[data-selected]:not(.range-middle)>*]:after:bg-zinc-950 dark:[&[data-disabled]>*]:after:bg-zinc-50/30",
+      "*:after:pointer-events-none *:after:absolute *:after:bottom-1 *:after:start-1/2 *:after:z-10 *:after:size-[3px] *:after:-translate-x-1/2 *:after:rounded-full *:after:bg-zinc-50 [&[data-selected]:not(.range-middle)>*]:after:bg-zinc-950 [&[data-disabled]>*]:after:bg-zinc-50/30 *:after:transition-colors",
     outside:
       "text-zinc-400 data-selected:bg-zinc-800/50 data-selected:text-zinc-400",
     hidden: "invisible",
