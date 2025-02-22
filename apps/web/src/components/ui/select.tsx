3004ecb4b50d3c7c136b40f0904c51be197f6b96
beambeambeam <supawitmarayat@gmail.com>
2025-02-22 13:57:20 +0700

    style: select input border

--- apps/web/src/components/ui/select.tsx
+++ apps/web/src/components/ui/select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "hover:bg-charcoal flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-white/10 bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-white transition-colors placeholder:text-white hover:cursor-pointer focus:outline-none focus:ring-1 focus:ring-white/30 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "hover:bg-charcoal border-vermilion-1 focus:ring-vermilion flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-white transition-colors placeholder:text-white hover:cursor-pointer focus:outline-none focus:ring-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className,
     )}
     {...props}
