7fdc2b3efc87d89604095419a416eddc4819c591
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2025-02-23 02:31:49 +0700

    fix: fix middleware

--- apps/server/src/auth/auth.middleware.ts
+++ apps/server/src/auth/auth.middleware.ts
@@ -8,8 +8,13 @@ export class AuthMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     if (req.cookies['sid']) {
       const session = await this.session.findOneById(req.cookies['sid']);
-      req['session_id'] = session.sid;
-      req['user_id'] = session.user_id;
+      if (session) {
+        req['session_id'] = session.sid;
+        req['user_id'] = session.user_id;
+      } else {
+        req['session_id'] = null;
+        req['user_id'] = null;
+      }
     } else {
       req['session_id'] = null;
       req['user_id'] = null;
