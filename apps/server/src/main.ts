3e6df01d22675afe9c9b602fa559002c90cd228c
beambeambeam <supawitmarayat@gmail.com>
2025-02-23 14:39:39 +0700

    chore: edit for staging

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -10,29 +10,30 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
-  const config = new DocumentBuilder()
-    .setTitle('Comcamp36 API')
-    .setDescription('Our lovely reference for Comcamp36 Website')
-    .setVersion('1.0')
-    .build();
+  if (process.env.NODE_ENV != 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Comcamp36 API')
+      .setDescription('Our lovely reference for Comcamp36 Website')
+      .setVersion('1.0')
+      .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
 
-  fs.writeFileSync('./openapi.json', JSON.stringify(documentFactory()));
+    fs.writeFileSync('./openapi.json', JSON.stringify(documentFactory()));
 
-  app.use(
-    '/reference',
-    apiReference({
-      spec: {
-        content: documentFactory,
-      },
-    }),
-  );
-
+    app.use(
+      '/reference',
+      apiReference({
+        spec: {
+          content: documentFactory,
+        },
+      }),
+    );
+  }
   app.useLogger(app.get(Logger));
 
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV != 'production') {
     app.enableCors({
       origin: 'http://localhost:3000',
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
@@ -40,7 +41,7 @@ async function bootstrap() {
     });
   } else {
     app.enableCors({
-      origin: 'https://comcamp.io',
+      origin: 'https://register.comcamp36.pages.dev/',
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
       credentials: true,
     });
