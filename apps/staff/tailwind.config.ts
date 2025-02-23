f4c108275cdfa46db5399f95a613f50ecb81eea4
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-02-23 14:57:50 +0700

    fix: staff app tw not working??

--- apps/staff/tailwind.config.ts
+++ apps/staff/tailwind.config.ts
@@ -12,6 +12,11 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
     },
   },
   plugins: [],
