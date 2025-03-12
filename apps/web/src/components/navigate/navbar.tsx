cf3f0f6939051c6ccc25b5b4880d1325acdc7b49
beambeambeam <supawitmarayat@gmail.com>
2025-03-12 20:52:42 +0700

    chore: remove unused

--- apps/web/src/components/navigate/navbar.tsx
+++ apps/web/src/components/navigate/navbar.tsx
@@ -11,7 +11,6 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import { calculateTimeLeft } from "@/libs/date";
 import { ReactNode } from "react";
 import { TextShimmerWave } from "../text/text-shimmer-wave";
 import { Banner } from "../ui/banner";
