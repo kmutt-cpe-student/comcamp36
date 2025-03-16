e398d5f5a620a902377a71f0146754eb28076618
beambeambeam <supawitmarayat@gmail.com>
2025-03-16 19:26:08 +0700

    feat: confetti firework

---
+++ apps/web/src/app/confirm/layout.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import LogoutBtn from "@/components/auth/logout-btn";
+import PolicyConsent from "@/components/card/policy-consent";
+import Footer from "@/components/navigate/footer";
+import Navbar from "@/components/navigate/navbar";
+import Spinner from "@/components/spinner";
+import { Button } from "@/components/ui/button";
+import { fetchClient, fetchQuery } from "@/libs/server/client";
+import { redirect, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+export default function RegisterLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [haveUser, setHaveUser] = useState(false);
+
+  const router = useRouter();
+  const { mutate } = fetchQuery.useMutation("post", "/auth/logout", {
+    onSuccess() {
+      toast.success("สำเร็จ!");
+      router.replace("/");
+    },
+    onError() {
+      router.replace("/");
+    },
+  });
+
+  useEffect(() => {
+    async function checkUser() {
+      try {
+        const { data } = await fetchClient.GET("/auth/me");
+        if (data?.email) {
+          setHaveUser(true);
+        } else {
+          return redirect("/sign-in");
+        }
+      } catch (error) {
+        console.error(error);
+        return redirect("/sign-in");
+      }
+    }
+    checkUser();
+  }, []);
+
+  if (!haveUser) {
+    return (
+      <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-[#0d0d0d] text-white">
+        <Spinner />
+        <Button
+          onClick={() => mutate({})}
+          variant="outline"
+          className="bg-[#0d0d0d]"
+        >
+          หากนานเกินไป ให้กดปุ่มนี้เพื่อย้อนกลับ
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-screen min-h-screen bg-[#0d0d0d] text-white">
+      <PolicyConsent />
+      <div className="absolute z-[100]">
+        <Navbar
+          items={[
+            {
+              href: "",
+              label: "",
+            },
+          ]}
+          extra={<LogoutBtn />}
+        />
+      </div>
+      <div className="font-prompt py-30 flex min-h-screen w-full justify-center px-0 sm:px-5">
+        {children}
+      </div>
+      <Footer />
+    </div>
+  );
+}
---
+++ apps/web/src/app/confirm/page.tsx
@@ -0,0 +1,10 @@
+import { ConfettiFireworks } from "@/app/confirm/_components/firework";
+
+function ConfirmPage() {
+  return (
+    <div className="pt-12">
+      <ConfettiFireworks />
+    </div>
+  );
+}
+export default ConfirmPage;
