d304a1b0dfb6b208f354be8082bef4852b08f370
beam <132804471+beambeambeam@users.noreply.github.com>
2025-03-22 12:29:35 +0700

    Confirmation components (#185)

---
+++ apps/web/src/app/confirm/_components/ineligible/index.tsx
@@ -0,0 +1,8 @@
+function Ineligible() {
+  return (
+    <div className="flex min-h-full w-full items-center justify-center">
+      Ineligible
+    </div>
+  );
+}
+export default Ineligible;
---
+++ apps/web/src/app/confirm/_components/reserved/index.tsx
@@ -0,0 +1,4 @@
+function Reserved() {
+  return <div className="flex items-center justify-center">Reserved</div>;
+}
+export default Reserved;
