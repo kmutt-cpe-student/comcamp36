d67fed7a1e0b061d946147f0f947e02e9b05bb1a
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2025-03-24 15:51:13 +0700

    fix: lib type

--- apps/web/src/libs/server/types.d.ts
+++ apps/web/src/libs/server/types.d.ts
@@ -79,6 +79,38 @@ export interface paths {
     patch?: never;
     trace?: never;
   };
+  "/users/info": {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    get?: never;
+    put?: never;
+    post: operations["UsersController_updateRegister"];
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
+  "/files/upload": {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    get?: never;
+    put?: never;
+    post: operations["FilesController_uploadFile"];
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
   "/files/upload-receipt": {
     parameters: {
       query?: never;
@@ -127,6 +159,22 @@ export interface paths {
     patch?: never;
     trace?: never;
   };
+  "/answer/submit-answer": {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    get?: never;
+    put?: never;
+    post: operations["AnswerController_submitAnswer"];
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
   "/answer/user-regis": {
     parameters: {
       query?: never;
@@ -136,7 +184,7 @@ export interface paths {
     };
     get: operations["AnswerController_findRegisWithUser"];
     put?: never;
-    post?: never;
+    post: operations["AnswerController_upsertRegisWithUser"];
     delete?: never;
     options?: never;
     head?: never;
@@ -152,7 +200,7 @@ export interface paths {
     };
     get: operations["AnswerController_findAcademicWithUser"];
     put?: never;
-    post?: never;
+    post: operations["AnswerController_upsertAcademicWithUser"];
     delete?: never;
     options?: never;
     head?: never;
@@ -247,6 +295,34 @@ export interface components {
       info_done: boolean;
       has_submit_answer: boolean;
     };
+    UpdateUserDto: {
+      title?: string;
+      fullname?: string;
+      age?: number;
+      birth?: string;
+      gender?: string;
+      religion?: string;
+      blood_group?: string;
+      graduation?: string;
+      school?: string;
+      course?: string;
+      telephone?: string;
+      medical_coverage?: string;
+      chronic_diseas?: string;
+      self_medicine?: string;
+      drug_allergic?: string;
+      food_allergic?: string;
+      prefer_food?: string;
+      address?: string;
+      home_phone_tel?: string;
+      comcamp_attendance?: boolean;
+      everyday_attendence?: boolean;
+      has_laptop?: boolean;
+      travel?: string;
+      parent_fullname?: string;
+      parent_relation?: string;
+      parent_phone?: string;
+    };
     UploadReceiptResponseDto: {
       receipt_key: string;
     };
@@ -288,8 +364,22 @@ export interface components {
       answer6_1: string;
       answer6_2: string;
     };
+    UpsertAnswerRegisDto: {
+      answer1: string;
+      answer2: string;
+      answer3: string;
+      answer4: string;
+      answer5: string;
+      answer6_1: string;
+      answer6_2: string;
+    };
     AnswerAcademicResponseDto: {
       id: string;
+      algo_answer: string;
+      chess_notation: string;
+      chess_score: number;
+    };
+    UpsertAnswerAcademicDto: {
       algo_answer: string;
       chess_notation: string;
       chess_score: number;
@@ -489,6 +579,61 @@ export interface operations {
       };
     };
   };
+  UsersController_updateRegister: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody: {
+      content: {
+        "application/json": components["schemas"]["UpdateUserDto"];
+      };
+    };
+    responses: {
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": components["schemas"]["UserResponseDto"];
+        };
+      };
+    };
+  };
+  FilesController_uploadFile: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody: {
+      content: {
+        "multipart/form-data": {
+          /** Format: binary */
+          face_photo?: File;
+          /** Format: binary */
+          thai_nationalid_copy?: File;
+          /** Format: binary */
+          parent_permission?: File;
+          /** Format: binary */
+          p1?: File;
+          /** Format: binary */
+          p7?: File;
+        };
+      };
+    };
+    responses: {
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+    };
+  };
   FilesController_uploadReceipt: {
     parameters: {
       query?: never;
@@ -557,6 +702,23 @@ export interface operations {
       };
     };
   };
+  AnswerController_submitAnswer: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      201: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+    };
+  };
   AnswerController_findRegisWithUser: {
     parameters: {
       query?: never;
@@ -576,6 +738,29 @@ export interface operations {
       };
     };
   };
+  AnswerController_upsertRegisWithUser: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody: {
+      content: {
+        "application/json": components["schemas"]["UpsertAnswerRegisDto"];
+      };
+    };
+    responses: {
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": components["schemas"]["AnswerRegisResponseDto"];
+        };
+      };
+    };
+  };
   AnswerController_findAcademicWithUser: {
     parameters: {
       query?: never;
@@ -584,6 +769,29 @@ export interface operations {
       cookie?: never;
     };
     requestBody?: never;
+    responses: {
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": components["schemas"]["AnswerAcademicResponseDto"];
+        };
+      };
+    };
+  };
+  AnswerController_upsertAcademicWithUser: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody: {
+      content: {
+        "application/json": components["schemas"]["UpsertAnswerAcademicDto"];
+      };
+    };
     responses: {
       200: {
         headers: {
